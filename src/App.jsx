56c44708286a9a8667261b46353adfd8e17ce14a
Amir9eng <amirmukhtarme@gmail.com>
2021-01-05 12:17:49 +0100

    coming-soon-page

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.scss";
+import logo from "./images/logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="logo" />
+      <p className="text-white text-4xl text-center my-8 react">
+        We are coming soon
+      </p>
+      <p className="text-white text-center node">
+        We are working very hard to add new features and improve the usability
+        of our site. <br /> In the meantime, scroll down to know a little more
+        about this new project.
+      </p>
+    </div>
+  );
+}
+
+export default App;
